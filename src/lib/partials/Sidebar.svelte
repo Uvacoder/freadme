ac708267485b946b4b01d177db9335ff7ba20054
nemo-omen <nemo.omen@gmail.com>
2021-07-09 07:58:32 -0500

    first commit

---
+++ src/lib/partials/Sidebar.svelte
@@ -0,0 +1,39 @@
+<script>
+
+</script>
+
+<aside id="sidebar">
+  <header class="sidebar-header">
+    <input type="text" class="subscribe-input" id="subscribe-input">
+    <label for="subscribe-input" class=sr-only>New Feed URL</label>
+    <button type="button" class="subscribe-submit">Save</button>
+  </header>
+</aside>
+
+<style>
+#sidebar {
+  order: 1;
+  height: 100%;
+  border-inline-end: 1px solid rgb(var(--primary-rgb), 0.3);
+  display: flex;
+  flex-direction: column;
+}
+
+.sidebar-header {
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-template-areas: "subscribe-input subscribe-submit";
+  grid-gap: 0.5rem;
+  padding: 1rem;
+  border-block-end: 1px solid rgb(var(--primary-rgb), 0.3);
+}
+
+.subscribe-input {
+  grid-area: subscribe-input;
+  background-color: rgb(var(--primary-rgb), 0.1);
+}
+
+.subscribe-submit {
+  grid-area: subscribe-submit;
+}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import '../lib/style/global.css';
+
+  import Sidebar from '../lib/partials/Sidebar.svelte';
+</script>
+<main>
+  <slot></slot>
+</main>
+
+<Sidebar />
+
+<style>
+  main {
+    order: 2;
+  }
+</style>
