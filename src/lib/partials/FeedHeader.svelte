cd44fb7b9384fbe282baa53fe370bc09cfcf4470
nemo-omen <nemo.omen@gmail.com>
2021-07-11 20:15:51 -0500

    Custom select element done

--- src/lib/partials/FeedHeader.svelte
+++ src/lib/partials/FeedHeader.svelte
@@ -2,14 +2,14 @@
 
 </script>
 
-<header id="feed-header" tabindex="0" labeledby="all-posts-title">
-  <label for="feed-filter-select">Feed Type</label>
+<header id="feed-header" labeledby="all-posts-title">
+  <label for="feed-filter-select" class="sr-only">Feed Type</label>
   <div class="select">
     <select name="feed-filter-select" id="feed-filter-select">
-      <option value="all">All</option>
-      <option value="unread">Unread</option>
-      <option value="read">Read</option>
-      <option value="favorites">Favorites</option>
+      <option value="all">All Posts</option>
+      <option value="unread">Unread Posts</option>
+      <option value="read">Read Posts</option>
+      <option value="favorites">Favorite Posts</option>
     </select>
   </div>
 </header>
@@ -26,10 +26,14 @@
     font-weight: 600;
     letter-spacing: 0.125rem;
     color: rgb(var(--light-rgb), 0.8);
+    order: 2;
   }
   #feed-header:focus {
     outline: 2px solid var(--accent-primary);
     outline-offset: -2px;
     background-color: rgb(var(--darkdark), 0.5);
+  }
+  .select {
+    order: 1;
   }
 </style>
