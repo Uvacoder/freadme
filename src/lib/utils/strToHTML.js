ed7d2fbfa0363b8431f872e023909469f0c715f2
nemo-omen <nemo.omen@gmail.com>
2021-07-12 14:56:27 -0500

    Remove broken code.

--- src/lib/utils/strToHTML.js
+++ src/lib/utils/strToHTML.js
@@ -1,25 +1,5 @@
-// const support = (function() {
-//   if(!window.DOMParser) return false;
-//   const parser = new window.DOMParser();
-//   try {
-//     parser.parseFromString('x', 'text/html');
-//   } catch(error) {
-//     return false;
-//   }
-//   return true;
-// })();
-
 export function strToHTML(htmlString) {
     const parser = new DOMParser();
     const doc = parser.parseFromString(htmlString, 'text/html');
     return doc.body.innerHTML;
-  // if(support) {
-  //   const parser = new DOMParser();
-  //   const doc = parser.parseFromString(htmlString, 'text/html');
-  //   return doc.body;
-  // }
-
-  // const dom = document.createElement('div');
-  // dom.innerHTML = htmlString;
-  // return dom;
 }
