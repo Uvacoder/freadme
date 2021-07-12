229db446cff214e77af5cfae9254ea27418ca855
nemo-omen <nemo.omen@gmail.com>
2021-07-12 17:43:15 -0500

    Clean up feed endpoint functions

--- src/routes/feed/index.json.js
+++ src/routes/feed/index.json.js
@@ -1,5 +1,6 @@
 import Parser from 'rss-parser';
 import { parseContent } from './parse.js';
+
 let parser = new Parser({
   customFields: {
     item: ['enclosure']
@@ -10,16 +11,9 @@ let parser = new Parser({
 const baseUrl = 'https://www.a11yproject.com/feed/feed.xml';
 
 export async function getFeed(address) {
-  const feed = await parser.parseURL(address);
-  
-  feed.items.map((item) => {
-    return {...item, ["content:encodedSnippet"]: parseContent(item["content:encodedSnippet"])};
-  })
-    
-    return feed;
+    return await parser.parseURL(address);
 }
-export const get = async() => {
-  const feed = await getFeed(baseUrl);
 
-  return {body: feed};
+export const get = async() => {
+  return {body: await getFeed(baseUrl)};
 };
