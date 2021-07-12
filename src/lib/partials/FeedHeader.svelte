fcdbfffd45953edc2e6ec3907e5a0fcd21536bb9
nemo-omen <nemo.omen@gmail.com>
2021-07-11 19:05:10 -0500

    Start custom select styling

--- src/lib/partials/FeedHeader.svelte
+++ src/lib/partials/FeedHeader.svelte
@@ -3,13 +3,29 @@
 </script>
 
 <header id="feed-header" tabindex="0" labeledby="all-posts-title">
-  <h2 id="all-posts-title">All Posts</h2>
+  <label for="feed-filter-select">Feed Type</label>
+  <div class="select">
+    <select name="feed-filter-select" id="feed-filter-select">
+      <option value="all">All</option>
+      <option value="unread">Unread</option>
+      <option value="read">Read</option>
+      <option value="favorites">Favorites</option>
+    </select>
+  </div>
 </header>
 
 <style>
   #feed-header {
-    padding: 0.5rem;
+    display: flex;
+    gap: 2rem;
+    padding: 0.5rem 2rem ;
     border-bottom: 1px solid rgb(var(--primary-rgb), 0.5);
+  }
+  label {
+    font-size: var(--size-600);
+    font-weight: 600;
+    letter-spacing: 0.125rem;
+    color: rgb(var(--light-rgb), 0.8);
   }
   #feed-header:focus {
     outline: 2px solid var(--accent-primary);
